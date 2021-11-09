318bf36e3487e0cc4801248aaddd6bb892583674
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-11-08 22:25:53 -0500

    ePayCo

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,11 @@
 const categories = require('../api/categories');
 const products = require('../api/products');
 const orders = require('../api/orders');
+const payments = require('../api/payments')
 
 module.exports = app => {
     app.use('/api/categories', categories);
     app.use('/api/products', products);
     app.use('/api/orders', orders);
+    app.use('/api/payments', payments);
 }
