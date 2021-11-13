5002da7bab4aa2ff731aaac83c95258b391e4155
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-11-13 02:21:51 -0500

    Products Crud

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-require('dotenv').config();
-const express = require('express');
-const expressConfig = require('./config/express');
-const routesConfig = require('./config/routes');
-const connectDB = require('./database');
+require("dotenv").config();
+const express = require("express");
+const expressConfig = require("./config/express");
+const routesConfig = require("./config/routes");
+const connectDB = require("./database");
 
 const app = express();
-
 connectDB();
 
 expressConfig(app);
 routesConfig(app);
 
 function startServer() {
-    const PORT = process.env.PORT || 4000
-    app.listen(PORT, () => console.log('Server running on port', PORT))
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log("Server running on port", PORT));
 }
 
 setImmediate(startServer);
