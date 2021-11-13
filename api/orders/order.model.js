00b206d23f90dcfbe2853b95d349faa21ce06b81
Felipe Cadavid <felipe.cadavid3@gmail.com>
2021-11-13 16:03:07 -0500

    Fixes to orders

--- api/orders/order.model.js
+++ api/orders/order.model.js
@@ -1,52 +1,68 @@
 const mongoose = require("mongoose");
 
-const orderSchema = mongoose.Schema({
-  // userId: {
-  //   type: mongoose.Schema.Types.ObjectId,
-  //   ref: 'User',
-  //   required: true,
-  //   default: null,
-  // },
-  orderDate: {
-    type: Date,
-    required: true
-  },
-  orderStatus: {
-    type: String,
-    enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
-    default: "pending",
-  },
-  orderItems: [
-    {
-      productId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Product",
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
-      },
+const orderSchema = mongoose.Schema(
+  {
+    // userId: {
+    //   type: mongoose.Schema.Types.ObjectId,
+    //   ref: 'User',
+    //   required: true,
+    //   default: null,
+    // },
+    orderDate: {
+      type: Date,
+      required: true,
     },
-  ],
-  shippingAddress: {
-    type: String,
-    required: true,
+    orderStatus: {
+      type: String,
+      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
+      default: "pending",
+    },
+    orderItems: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    shippingAddress: {
+      type: String,
+      required: true,
+    },
+    shippingCity: {
+      type: String,
+      required: true,
+    },
+    clientPhone: {
+      type: Number,
+      required: true,
+    },
+    clientEmail: {
+      type: String,
+      required: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+    },
+    shippingTime: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
   },
-  shippingExtraInfo: String,
-  shippingCity: {
-    type: String,
-    required: true,
-  },
-  clientPhone: {
-    type: Number,
-    required: true,
-  },
-  clientEmail: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const orderModel = mongoose.model("Order", orderSchema);
 
